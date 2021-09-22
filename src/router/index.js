62161ab4dd12635f7672151eef56e4e426d0a0c0
Ian <yetian.su@gmail.com>
2021-09-22 22:03:59 +0800

    created three tag, money, labels, statistics

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Money from "../views/Money";
+import Statistics from "../views/Statistics";
+import Labels from "../views/Labels";
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/Money',
+        name: 'Money',
+        component: Money
+    },
+    {
+        path: '/Label',
+        name: 'Label',
+        component: Labels
+    },
+    {
+        path: '/Statistics',
+        name: 'Statistics',
+        component: Statistics
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
